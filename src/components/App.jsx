e7dfffd7329b7bc93c8b42d004201e8960afbec1
Aqil Thanawala <aqilt@Aqils-MacBook-Pro.local>
2019-01-17 12:33:53 -0800

    created youtube API need to implement dynamic behavior for search

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import exampleVideoData from '../data/exampleVideoData.js';
 import VideoList from './VideoList.js';
 import VideoPlayer from './VideoPlayer.js';
+import Search from './Search.js';
 
 class App extends React.Component {
   constructor() {
@@ -8,6 +9,7 @@ class App extends React.Component {
     this.state = {
       aState: false,
       videoNum: 0,
+
     };
   }
 
@@ -22,7 +24,8 @@ class App extends React.Component {
       <div>
         <nav className="navbar">
           <div className="col-md-6 offset-md-3">
-            <div><h5><em>search</em> view goes here</h5></div>
+            <Search />
+            {/* <div><h5><em>search</em> view goes here</h5></div> */}
           </div>
         </nav>
         <div className="row">
--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,15 @@
 var searchYouTube = (options, callback) => {
-  // TODO
+  //{query, max, key} {q, maxResults: 5}
+
+  return $.get("https://www.googleapis.com/youtube/v3/search", callback(data));
+  // let searchData = () => {
+  //   return $.ajax({
+  //     url: 'https://www.googleapis.com/youtube/v3/search',
+  //     type: 'GET',
+  //     data: ,
+  //     contentType: ''
+  //   });
+  // }
 };
 
 export default searchYouTube;
