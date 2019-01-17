52824724ad4653cbac26a515d42371a8a0867d32
Aqil Thanawala <aqilt@Aqils-MacBook-Pro.local>
2019-01-16 16:47:29 -0800

    refactored videolist and videolistentry as needed to pass all mocha tests

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,4 @@
+import exampleVideoData from '../data/exampleVideoData.js';
 import VideoList from './VideoList.js';
 
 var App = () => (
@@ -12,7 +13,7 @@ var App = () => (
         <div><h5><em>videoPlayer</em> view goes here</h5></div>
       </div>
       <div className="col-md-5">
-        <VideoList />
+        <VideoList videos={exampleVideoData} />
         {/* <div><h5><em>videoList</em> view goes here</h5></div> */}
       </div>
     </div>
